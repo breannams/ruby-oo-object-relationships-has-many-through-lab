8ee53edc387743f5807e52e212c6057cac8b0c3b
breannams <breannamarie_s@hotmail.com>
2021-01-16 22:43:45 -0500

    commit tree 02884c10c3d76b1e3c0b9382fe8ee538e1c86e39

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,31 @@
+class Doctor
+    attr_accessor :name
+    @@all = []
 
+    def initialize (name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def new_appointment(date,patient)
+        Appointment.new(date, patient, self)
+    end
+    
+    def appointments
+        Appointment.all.select do |appointments|
+            appointments.doctor == self
+        end
+    end
+   
+    
+    def patients
+        appointments.map do |app|
+            app.patient
+        end
+    end
+
+end
